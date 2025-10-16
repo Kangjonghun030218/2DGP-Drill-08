6ce5f16ed339ec3f406ac1f311488e506647db7d
kANG JONGHUN <2424kang@tukorea.ac.kr>
2025-10-16 13:51:03 +0900

    Sleep Class 정의 완료

--- boy.py
+++ boy.py
@@ -4,6 +4,46 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
 from state_machine import StateMachine
 
 
+class Sleep:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self):
+        self.boy.dir = 0
+
+    def exit(self):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 300, 100, 100, 3.141592/2,'',self.boy.x-25, self.boy.y-25,100,100)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 200, 100, 100,3.141592/2,'', self.boy.x+25, self.boy.y-25,100,100)
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.face_dir = 1
+        self.dir = 0
+        self.image = load_image('animation_sheet.png')
+
+        self.IDLE = Idle(self)
+        self.state_machine = StateMachine(self.IDLE)
+
+    def update(self):
+        self.state_machine.update()
+
+
+    def draw(self):
+        self.state_machine.draw()
+
+
 class Idle:
 
     def __init__(self, boy):
@@ -34,7 +74,8 @@ class Boy:
         self.image = load_image('animation_sheet.png')
 
         self.IDLE = Idle(self)
-        self.state_machine = StateMachine(self.IDLE)
+        self.SLEEP = Sleep(self)
+        self.state_machine = StateMachine(self.SLEEP)
 
     def update(self):
         self.state_machine.update()
